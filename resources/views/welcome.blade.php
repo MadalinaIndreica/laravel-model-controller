8edd7430616fe08e40469e073ebe53e36540dd36
MadalinaIndreica <madalinaindreicamaddy4@gmail.com>
2024-01-15 16:07:26 +0100

    add: movies's cards

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Welcome to laravel</h1>
+    <h1>Movies</h1>
+    <div class="container">
+        <div class="row row-cols-6 d-flex flex-wrap">
+            <div class="col h-100">
+                @foreach ($movie as $movie)
+                    <div class="card" style="width: 18rem;">
+                      <img src="{{ $movie->image }}" class="card-img-top" alt="">
+                        <div class="card-body">
+                            <h3>{{ $movie->title }}</h3>
+                            <p class="card-text">{{ $movie->id }}</p>
+                            
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
 @endsection
