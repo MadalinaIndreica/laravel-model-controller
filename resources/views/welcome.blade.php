c47ec062fea5015af92f6cc88752c59971f6c791
MadalinaIndreica <madalinaindreicamaddy4@gmail.com>
2024-01-15 16:42:16 +0100

    add: layout and some details

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,21 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Movies</h1>
     <div class="container">
-        <div class="row row-cols-6 d-flex flex-wrap">
-            <div class="col h-100">
-                @foreach ($movie as $movie)
-                    <div class="card" style="width: 18rem;">
-                      <img src="{{ $movie->image }}" class="card-img-top" alt="">
-                        <div class="card-body">
-                            <h3>{{ $movie->title }}</h3>
-                            <p class="card-text">{{ $movie->id }}</p>
-                            
+        <h1 class="text-center my-title">Movies</h1>
+        <h2 class="text-white">Top 10 dei film in Italia oggi:</h2>
+        <div class="wrapper">
+            <div class="row row-cols-auto">
+                <div class="col d-flex flex-wrap gap-3">
+                    @foreach ($movie as $movie)
+                        <div class="card" style="width: 14rem;">
+                            <img src="{{ $movie->image }}" class="card-img-top" alt="">
+                            <div class="card-body">
+                                <h3>{{ $movie->title }}</h3>
+                                <p class="card-text"> <strong>Titolo originale:</strong>  {{ $movie->original_title }}</p>
+                                <p class="card-text"> <strong>Data di uscita:</strong>  {{ $movie->date }}</p>
+                                <p class="card-text"> <strong>Voto:</strong>  {{ $movie->vote }}</p>
+                            </div>
                         </div>
-                    </div>
-                @endforeach
+                    @endforeach
+                </div>
             </div>
-        </div>
+       </div>
     </div>
 @endsection
